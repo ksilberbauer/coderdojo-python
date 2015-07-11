9b7858aa3f58bb338ac5b0151ac79c15f3db298a
Kyle Silberbauer <kyle.silberbauer@outlook.com>
2015-07-10 22:00:03 -0400

    dictionary

--- pet/pet.py
+++ pet/pet.py
@@ -1,18 +1,20 @@
-name = input("What is your pet's name?")
-age = 5
-weight = 9
-hungry = True
-photo = '(=^o.o^=)__'
+pet = {
+    'age': 5,
+    'weight': 9,
+    'hungry': True,
+    'photo': '(=^o.o^=)__',
+    'favorite foods': ['milk', 'fish', 'lasagna', 'cat food']
+}
 
-favorite_foods = ['milk', 'fish', 'lasagna', 'cat food']
+pet['name'] = input("What is your pet's name?")
 
-print 'Hello, my name is ' + name
-print photo
+print 'Hello, my name is ' + pet['name']
+print pet['photo']
 
-if hungry:
+if pet['hungry']:
     print "I'm hungry!"
     print "My favorite foods are:"
-    for food in favorite_foods:
+    for food in pet['favorite foods']:
         print food
     print "Can I have one of those?"
 else:
