8ae1608a183a5f9583adec7e235bd3d33aab8bf3
Kyle Silberbauer <kyle.silberbauer@outlook.com>
2015-07-10 21:55:46 -0400

    for loop + list

--- pet/pet.py
+++ pet/pet.py
@@ -4,10 +4,16 @@ weight = 9
 hungry = True
 photo = '(=^o.o^=)__'
 
+favorite_foods = ['milk', 'fish', 'lasagna', 'cat food']
+
 print 'Hello, my name is ' + name
 print photo
 
 if hungry:
     print "I'm hungry!"
+    print "My favorite foods are:"
+    for food in favorite_foods:
+        print food
+    print "Can I have one of those?"
 else:
     print "I just ate"
