893bee0f3e3556df8d11a9402bd9c54a6f14573e
Kyle Silberbauer <kyle.silberbauer@outlook.com>
2015-07-10 22:04:05 -0400

    function + while loop

--- pet/pet.py
+++ pet/pet.py
@@ -1,4 +1,4 @@
-pet = {
+my_pet = {
     'age': 5,
     'weight': 9,
     'hungry': True,
@@ -6,16 +6,23 @@ pet = {
     'favorite foods': ['milk', 'fish', 'lasagna', 'cat food']
 }
 
-pet['name'] = input("What is your pet's name?")
+my_pet['name'] = input("What is your pet's name?")
 
-print 'Hello, my name is ' + pet['name']
-print pet['photo']
+# This function works for any dictionary with 'hungry' and 'favorite foods' properties
+def feed(pet):
+    favorite_foods = pet['favorite foods']
+    if pet['hungry']:
+        print "I'm hungry!"
+        while input("What do you want to feed your pet?") not in favorite_foods:
+            print "I don't like that food. I want one of these:"
+            for food in favorite_foods:
+                print food
+        pet['hungry'] = False
+        print "Mmm, yummy, thanks!"
+    else:
+        print "I just ate"
 
-if pet['hungry']:
-    print "I'm hungry!"
-    print "My favorite foods are:"
-    for food in pet['favorite foods']:
-        print food
-    print "Can I have one of those?"
-else:
-    print "I just ate"
+print 'Hello, my name is ' + my_pet['name']
+print my_pet['photo']
+
+feed(my_pet)
