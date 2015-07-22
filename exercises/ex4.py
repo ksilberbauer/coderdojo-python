fc944e6b1de65be2a2432b3f1b445f2ee28923da
Kyle Silberbauer <kyle.silberbauer@outlook.com>
2015-07-21 23:34:36 -0400

    challenge 4

---
+++ exercises/ex4.py
@@ -0,0 +1,6 @@
+from student_generator import get_students, years
+
+# create the following functions:
+# TODO: takes a list of students, and returns their names in alphabetical order. hint: list.sort()
+# TODO: takes a list of students, and returns the number of students by year. bonus: bar chart!
+# TODO: given a student name, create a function that returns their average grade. hint: 2 functions, one to find the student, one to calculate an average
